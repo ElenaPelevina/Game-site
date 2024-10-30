85a8323be8913c65a46069ee0dd606fe349f792a
ElenaPelevina <elena.al.pelevina@gmail.com>
2024-10-30 23:44:27 +0300

    Added js

---
+++ script.js
@@ -0,0 +1,54 @@
+// Task 1//
+let a = 10;
+alert(a);
+a = 20
+alert(a);
+
+//Task 2//
+let yearOfFirstIPhone = 2007
+alert(yearOfFirstIPhone);
+
+//Task 3//
+let javaScriptCreator = 'Brendon Eich';
+alert (javaScriptCreator);
+
+//Task 4//
+a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+//Task 5//
+let result = b ** 5;
+alert(result);
+
+//Task 6//
+a = 9;
+alert(a % b);
+
+//Task 7//
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//Task 8//
+let age = Number(prompt("Сколько вам лет?"));
+alert(age);
+
+//Task 9//
+const user = {
+    name: 'Jane',
+    age: 12,
+    isAdmin: false
+}
+
+//Task 10//
+const name = prompt('Как Вас зовут?');
+alert(`Привет, ${name}!`);
