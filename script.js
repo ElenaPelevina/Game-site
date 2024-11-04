63862961cc58461671798175740630990c2edb8d
ElenaPelevina <elena.al.pelevina@gmail.com>
2024-11-04 18:02:52 +0300

    added js

--- script.js
+++ script.js
@@ -69,12 +69,18 @@ switch (monthNumber) {
 
 //Addtitonal//
 //Task 1//
-let number = Number(prompt('Пожалуйста, введите любое число'));
-if (number % 2 === 0) {
+let number = prompt('Пожалуйста, введите любое число');
+let result = Number(number)
+if (isNaN(result)) {
+alert('Not a number')
+} else {
+alert("It's a Number!")
+if (result % 2 === 0) {
     alert('Число четное')
 } else {
     alert('Число нечетное')
 }
+};
 
 //Task 2//
 let clientOS = prompt('Введите OS Вашего телефона');
