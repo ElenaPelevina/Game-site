1e30448dec8ffd90dd0f7dc82bd2914c76c93ddb
ElenaPelevina <elena.al.pelevina@gmail.com>
2024-11-30 12:27:29 +0300

    Added Tasks

--- script.js
+++ script.js
@@ -211,17 +211,73 @@ const letsRockPaperScissors = () => {
     } ;
  
 
-    //Task 1
+//Task 1
+const toCompare = (a, b) => {
+        return a.age - b.age 
+        }
+        
+const people = [
+           { name: 'Глеб', age: 29 },
+           { name: 'Анна', age: 17 },
+           { name: 'Олег', age: 7 },
+           { name: 'Оксана', age: 47 }
+        ];
+        
+console.log(people.sort(toCompare));
 
-    //Task 2
+//Task 2
+function isPositive(a) {
+    if (a < 0) {return false}
+    else {return true}
+}
 
-    //Task 3
+function filter(arr, ruleFunction) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+    if (ruleFunction(arr[i]) === true) {
+    newArr.push(arr[i])
+    }
+    }
+    return newArr
+} 
 
-    //Task 4
-    function delayForSecond(callback) {
-        setTimeout(callback , 1000);
+console.log(filter([3, -4, 1, 9], isPositive));
+
+
+//Task 3
+
+let date = new Date;
+const timer = setInterval(() => console.log(date), 3000 );
+setTimeout(() => { clearInterval(timer); console.log('30 секунд прошло'); }, 3000 * 10);
+
+//Task 4
+    
+function delayForSecond(callback) {
+
+    setTimeout(callback , 1000);
         
     }
-    delayForSecond(function () {
+
+delayForSecond(function () {
        console.log('Привет, Глеб!');
     })  
+
+//Task 5
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log('Привет, ${name}!');
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(() => {sayHi('Глеб')}) 
